a4b37edd426c2e5509fbf68e198e90ae813ffb3c
Victor Korobkovsky <vitkorob@gmail.com>
2018-03-12 22:34:04 +0300

    Fixes bug. Initial value of SystemCoreClock must be equal HSI frequency

--- Drivers/STM32F1xx_LL_Driver/Source/system_stm32f1xx.c
+++ Drivers/STM32F1xx_LL_Driver/Source/system_stm32f1xx.c
@@ -133,12 +133,7 @@
 /*******************************************************************************
 *  Clock Definitions
 *******************************************************************************/
-#if defined(STM32F100xB) ||defined(STM32F100xE)
-  uint32_t SystemCoreClock         = 24000000U;        /*!< System Clock Frequency (Core Clock) */
-#else /*!< HSI Selected as System Clock source */
-  uint32_t SystemCoreClock         = 72000000U;        /*!< System Clock Frequency (Core Clock) */
-#endif
-
+uint32_t SystemCoreClock = 8000000U;  /*!< System Clock Frequency (Core Clock) */
 const uint8_t AHBPrescTable[16U] = {0, 0, 0, 0, 0, 0, 0, 0, 1, 2, 3, 4, 6, 7, 8, 9};
 const uint8_t APBPrescTable[8U] =  {0, 0, 0, 0, 1, 2, 3, 4};
 
